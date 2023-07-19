ea96ed1609e9b62eb534a6ac37b10f5d8b12625c
cnwl12 <ccjo2@daum.net>
2023-07-19 20:22:42 +0900

    0719_DB연동완료(추지)

---
+++ src/main/java/com/itwillbs/service/MemberService.java
@@ -0,0 +1,30 @@
+package com.itwillbs.service;
+
+import java.sql.Timestamp;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import com.itwillbs.dao.MemberDAO;
+import com.itwillbs.domain.MemberDTO;
+
+@Service
+public class MemberService {
+	
+	@Inject
+	private MemberDAO memberDAO;
+
+	public void insertMember(MemberDTO memberDTO) {
+		System.out.println("MemberService insertMember() 확인!!!");
+		
+		// 날짜 저장 (테스트 dto에서는 date 지웠음)
+		// memberDTO.setDate(new Timestamp(System.currentTimeMillis()));
+		
+		// insertMember 메서드 호출
+		memberDAO.insertMember(memberDTO);
+	}	
+		
+		
+
+}
