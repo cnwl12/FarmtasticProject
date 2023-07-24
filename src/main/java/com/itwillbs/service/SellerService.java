94c847aa580f16e9277edd150434b6ebca6796d8
cnwl12 <ccjo2@daum.net>
2023-07-24 10:44:27 +0900

    확인 추지

--- src/main/java/com/itwillbs/service/SellerService.java
+++ src/main/java/com/itwillbs/service/SellerService.java
@@ -48,7 +48,8 @@ public class SellerService {
                         String filePath = uploadPath + "/" + storedFileName;
                         
                         System.out.println("filePath : " + filePath);
-
+                        
+                        // 서버랑 이름 맞춰줘야함
                         String saveFileName = "http://c2d2303t2.itwillbs.com/FarmProject/resources/upload/" + storedFileName;
 
                         // 임시경로에서 filePath로 파일이동 
