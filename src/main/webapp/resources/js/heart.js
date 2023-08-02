89114d017d1a10b0cc3a72e23918a37d9bbeca10
김서영 <qaztjudtjdud@naver.com>
2023-08-02 19:19:26 +0900

    0802 찜하기 색넣음

--- src/main/webapp/resources/js/heart.js
+++ src/main/webapp/resources/js/heart.js
@@ -1,25 +1,25 @@
 /**
  * 
  */
- document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
   // 페이지 로드 완료 시 모든 위시리스트 버튼 참조
   var wishlistBtns = document.querySelectorAll(".wishlist-btn");
 
   // 각 버튼에 대한 이벤트 리스너 추가
   Array.prototype.forEach.call(wishlistBtns, function (btn) {
     btn.addEventListener("click", function (event) {
-      var member_num = event.target.parentElement.getAttribute("data-member-num");
-      var item_num = event.target.parentElement.getAttribute("data-item-num");
+      var member_num = this.getAttribute("data-member-num");
+      var item_num = this.getAttribute("data-item-num");
 
       add(event, member_num, item_num);
     });
   });
 });
- 
+
 
   function add(event, member_num, item_num) {
   // 함수 본문 작성
- 	if (member_num == null || member_num == "") {
+ 	if (member_num == null || member_num.length == 0) {
     alert("로그인이 필요한 서비스입니다.");
     window.location.href = "login";
   } else {
@@ -34,8 +34,10 @@
       success: function (response) {
         // 요청이 성공적으로 완료된 경우 실행할 코드
         if (response.message == "찜 목록에 상품이 추가되었습니다.") {
+          event.target.classList.add("red"); 
           alert(response.message);
         } else {
+          event.target.classList.remove("red"); 
           alert("찜 목록에서 상품을 삭제했습니다.");
         }
       },
